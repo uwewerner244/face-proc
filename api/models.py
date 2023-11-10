3ff21171eddad1af2410db5ad3b4496d02002718
uwewerner <uwewerner244@gmail.com>
2023-11-10 10:34:44 +0500

    Deleting code

--- api/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class TestModel(models.Model):
-    digit = models.JSONField()
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from api.views import LogoutView, TokenAPIView
-
-
-urlpatterns = [
-    path("login/", TokenAPIView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-]
