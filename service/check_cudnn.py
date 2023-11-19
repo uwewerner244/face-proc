e429d5e53c39811b779aa4aa98185f1a5f8acb9b
uwewerner <uwewerner244@gmail.com>
2023-11-20 03:24:32 +0500

    Cleaning up the system storage

--- service/check_cudnn.py
+++
@@ -1,3 +0,0 @@
-import tensorflow as tf
-print(tf.sysconfig.get_build_info()["cuda_version"])
-print(tf.sysconfig.get_build_info()["cudnn_version"])
--- service/tasks.py
+++
@@ -1,4 +0,0 @@
-import random
-
-lst = [random.randint(0, 100) for _ in range(100000)]
-print(sum(lst) / len(lst))
