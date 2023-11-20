85117f0a5d6b5a730a0f62e54969e1b0ec9d0d9e
uwewerner <uwewerner244@gmail.com>
2023-11-20 09:32:56 +0500

    send continuous frame to web client in DRF

--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
+from django.http import StreamingHttpResponse
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 
 from api.models import Employee, Camera
 from api.serializers import EmployeeSerializer, CameraSerializer
+
+from imutils.video import VideoStream
+import cv2
+import time
 
 
 class EmployeeViewSet(ModelViewSet):
@@ -12,3 +18,31 @@ class EmployeeViewSet(ModelViewSet):
 class CameraViewSet(ModelViewSet):
     queryset = Camera.objects.all()
     serializer_class = CameraSerializer
+
+
+class VideoStreamAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        url = request.query_params.get('url')
+        if not url:
+            return StreamingHttpResponse('URL parameter is missing', status=400, content_type='text/plain')
+
+        vs = VideoStream(url).start()
+        time.sleep(0.5)  # Warm-up time for the camera
+
+        def frame_generator():
+            while True:
+                frame = vs.read()
+                if frame is None:
+                    break
+
+                (flag, encodedImage) = cv2.imencode(".jpg", frame)
+                if not flag:
+                    continue
+
+                yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                       bytearray(encodedImage) + b'\r\n')
+
+        return StreamingHttpResponse(
+            frame_generator(),
+            content_type='multipart/x-mixed-replace; boundary=frame'
+        )
--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from api.views import EmployeeViewSet, CameraViewSet
+from api.views import EmployeeViewSet, CameraViewSet, VideoStreamAPIView
 
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet)
@@ -11,4 +11,6 @@ router.register(r'cameras', CameraViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/stream/', VideoStreamAPIView.as_view(), name='stream'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
